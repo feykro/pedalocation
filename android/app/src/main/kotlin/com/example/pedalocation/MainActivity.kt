5f155954781ec23732575cb64597917ce3848567
Tom Perin <57326877+feykro@users.noreply.github.com>
2024-07-21 16:19:38 +0200

    Init commit

---
+++ android/app/src/main/kotlin/com/example/pedalocation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pedalocation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+const minigolfBlue = Color.fromARGB(255, 88, 179, 198);
+const minigolfBlueAlt = Color(0xFF8DE7FA);
+const minigolPink = Color.fromARGB(255, 222, 158, 180);
